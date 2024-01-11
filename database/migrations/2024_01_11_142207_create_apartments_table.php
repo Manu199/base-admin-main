68dd957d2f15c39134849d1a3a17faa9f32fe76f
Manu199 <manu199gh@gmail.com>
2024-01-11 16:51:32 +0100

    [add] migrations

---
+++ database/migrations/2024_01_11_142207_create_apartments_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('cascade');
+
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+
+            $table->timestamps();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->decimal('price', 6, 2)->unsigned();
+            $table->integer('square_meters')->unsigned();
+            $table->integer('num_of_room')->unsigned();
+            $table->integer('num_of_bed')->unsigned();
+            $table->integer('num_of_bathroom')->unsigned();
+            $table->string('country');
+            $table->string('street_address');
+            $table->string('city_name');
+            $table->string('postal_code');
+            $table->decimal('lat', 7, 5);
+            $table->decimal('lon', 8, 5);
+            $table->string('image_url');
+            $table->boolean('visible');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
