150b52cf7db194918d76e6864578157a83f531ec
MrArcano <salvatore-dario@hotmail.it>
2024-01-12 12:38:19 +0100

    [add]: apartments_data for seeder

---
+++ database/seeders/apartments_data.php
@@ -0,0 +1,10 @@
+<?php
+
+$data = [
+    // Inserisci qui l'array di dati degli appartamenti
+];
+
+return $data;
+
+// copia il codice sotto dentro il file ApartmentSeeder.php
+// $apartmentsData = include 'apartments_data.php';
