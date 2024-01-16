f56e2e90b51f7010888c1e1fe1494da58b7b6b8d
Manu199 <manu199gh@gmail.com>
2024-01-16 09:52:01 +0100

    [add] profile menu

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -3,7 +3,8 @@
     <div class=" col-2">
         <ul class="nav ps-4">
             <li class="nav-item">
-                <a class="nav-link btn fw-bold btn-header {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : ''}}" href="{{ route('admin.home') }}">Home</a>
+                <a class="nav-link btn fw-bold btn-header {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : '' }}"
+                    href="{{ route('admin.home') }}">Home</a>
             </li>
         </ul>
 
@@ -11,13 +12,46 @@
     <div class="col-6 ">
         <ul class="nav justify-content-end pe-4">
             <li class="nav-item mx-3">
-                <a class="nav-link btn fw-bold btn-header {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : ''}}" href="{{ route('admin.apartment.index') }}">Apartments List</a>
+                <a class="nav-link btn fw-bold btn-header {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : '' }}"
+                    href="{{ route('admin.apartment.index') }}">Apartments List</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link btn fw-bold btn-header-2 {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : ''}}" href="{{ route('admin.apartment.create') }}"><i class="fa-solid fa-plus"></i> New Apartment</a>
+                <a class="nav-link btn fw-bold btn-header-2 {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : '' }}"
+                    href="{{ route('admin.apartment.create') }}"><i class="fa-solid fa-plus"></i> New Apartment</a>
             </li>
-        </ul>
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
 
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
+                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
     </div>
     {{-- <ul class="nav justify-content-center gap-2">
         <li class="nav-item">
@@ -30,4 +64,5 @@
             <a class="nav-link btn fw-bold btn-header-2 {{ Route::currentRouteName() === 'home' ? 'text-bg-info' : ''}}" href="{{ route('admin.apartment.create') }}"><i class="fa-solid fa-plus"></i> New Apartment</a>
         </li>
     </ul> --}}
+
 </header>
