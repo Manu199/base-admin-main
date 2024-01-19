4a8f9e46d2b0b9b261f8b1a8e733e9516f4b82db
MrArcano <salvatore-dario@hotmail.it>
2024-01-19 18:28:36 +0100

    [edit]: validate form register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+    .icon-input{
+        position: absolute;
+        top: 50%;
+        right: 25px;
+        transform: translate(-50%,-50%)
+    }
+</style>
+
+
     <div class="container my-5">
         <div class="card-login m-auto">
             <div class="row justify-content-center">
@@ -10,15 +21,16 @@
                         <div class="card-header text-center bg-white border-0 my-3">{{ __('Register') }}</div>
 
                         <div class="card-body">
-                            <form method="POST" action="{{ route('register') }}" onsubmit="return validatePasswords()">
+                            <form method="POST" action="{{ route('register') }}" onsubmit="return validateForm()">
                                 @csrf
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="name" type="text" placeholder="Name"
-                                            class="form-control custom-checkbox @error('name') is-invalid @enderror"
+                                            class="form-control custom-checkbox pe-5 @error('name') is-invalid @enderror"
                                             name="name" value="{{ old('name') }}" required autocomplete="name"
                                             autofocus>
+                                        <span class="icon-input" id="icon-name"></span>
 
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
@@ -29,11 +41,13 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="lastname" type="text" placeholder="Lastname"
-                                            class="form-control custom-checkbox @error('lastname') is-invalid @enderror"
+                                            class="form-control custom-checkbox pe-5 @error('lastname') is-invalid @enderror"
                                             name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname"
                                             autofocus>
+                                        <span class="icon-input" id="icon-lastname"></span>
+
 
                                         @error('lastname')
                                             <span class="invalid-feedback" role="alert">
@@ -44,7 +58,7 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col ">
                                         <input id="date_of_birth" type="date"
                                             class="form-control custom-checkbox @error('date_of_birth') is-invalid @enderror"
                                             name="date_of_birth" value="{{ old('date_of_birth') }}"
@@ -60,11 +74,12 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="phone_number" type="tel" placeholder="Tel +00 123456789"
-                                            class="form-control custom-checkbox @error('phone_number') is-invalid @enderror"
+                                            class="form-control custom-checkbox pe-5 @error('phone_number') is-invalid @enderror"
                                             name="phone_number" value="{{ old('phone_number') }}" required
                                             autocomplete="phone_number" autofocus>
+                                        <span class="icon-input" id="icon-phone_number"></span>
 
                                         @error('phone_number')
                                             <span class="invalid-feedback" role="alert">
@@ -75,10 +90,11 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="email" placeholder="E-Mail Address" type="email"
-                                            class="form-control custom-checkbox @error('email') is-invalid @enderror"
+                                            class="form-control custom-checkbox pe-5 @error('email') is-invalid @enderror"
                                             name="email" value="{{ old('email') }}" required autocomplete="email">
+                                            <span class="icon-input" id="icon-email"></span>
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -89,10 +105,11 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="password" placeholder="Password" type="password"
-                                            class="form-control custom-checkbox @error('password') is-invalid @enderror" name="password"
+                                            class="form-control custom-checkbox pe-5 @error('password') is-invalid @enderror" name="password"
                                             required autocomplete="new-password">
+                                            <span class="icon-input" id="icon-password"></span>
 
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
@@ -103,11 +120,11 @@
                                 </div>
 
                                 <div class="mb-4 row justify-content-center">
-                                    <div class="col">
+                                    <div class="col position-relative">
                                         <input id="password-confirm" placeholder="Password confirm" type="password"
-                                            class="form-control custom-checkbox" name="password_confirmation" required
+                                            class="form-control custom-checkbox pe-5" name="password_confirmation" required
                                             autocomplete="new-password">
-                                            <span id="password-error" class="text-danger"></span>
+                                            <span class="icon-input" id="icon-password-confirm"></span>
                                     </div>
                                 </div>
 
@@ -127,18 +144,113 @@
     </div>
 
     <script>
-        function validatePasswords() {
-            let password = document.getElementById('password').value;
-            let confirmPassword = document.getElementById('password-confirm').value;
-            let errorSpan = document.getElementById('password-error');
-
-            if (password !== confirmPassword) {
-                errorSpan.textContent = 'Le password non corrispondono.';
-                return false;
+        // Name
+        const name = document.getElementById('name');
+        name.addEventListener('input', function(){
+            const iconName = document.getElementById('icon-name');
+            if(name.value.length < 1 ){
+                iconName.innerHTML = '&#x2718;';
+                iconName.classList.add('text-danger');
+                iconName.classList.remove('text-success');
+            }else{
+                iconName.innerHTML = '&#x2714;';
+                iconName.classList.add('text-success');
+                iconName.classList.remove('text-danger');
             }
+        });
 
-            return true;
+        // Lastname
+        const lastname = document.getElementById('lastname');
+        lastname.addEventListener('input', function(){
+            const iconLastname = document.getElementById('icon-lastname');
+            if(lastname.value.length < 1 ){
+                iconLastname.innerHTML = '&#x2718;';
+                iconLastname.classList.add('text-danger');
+                iconLastname.classList.remove('text-success');
+            }else{
+                iconLastname.innerHTML = '&#x2714;';
+                iconLastname.classList.add('text-success');
+                iconLastname.classList.remove('text-danger');
+            }
+        });
+
+        // Phone Number
+        const phoneNumber = document.getElementById('phone_number');
+        phoneNumber.addEventListener('input', function(){
+            const icon = document.getElementById('icon-phone_number');
+            if (phoneNumber.value[0] === '+' && /^\+\d*$/.test(phoneNumber.value)) {
+                icon.innerHTML = '&#x2714;';
+                icon.classList.add('text-success');
+                icon.classList.remove('text-danger');
+            } else {
+                icon.innerHTML = '&#x2718;';
+                icon.classList.add('text-danger');
+                icon.classList.remove('text-success');
+            }
+        });
+
+        // Email
+        const email = document.getElementById('email');
+        email.addEventListener('input', function(){
+            const icon = document.getElementById('icon-email');
+            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+            if(emailRegex.test(email.value)){
+                icon.innerHTML = '&#x2714;';
+                icon.classList.add('text-success');
+                icon.classList.remove('text-danger');
+            }else{
+                icon.innerHTML = '&#x2718;';
+                icon.classList.add('text-danger');
+                icon.classList.remove('text-success');
+            }
+        });
+
+        // Password
+        const password = document.getElementById('password');
+        password.addEventListener('input', function(){
+            const icon = document.getElementById('icon-password');
+            if(password.value.length >= 8){
+                icon.innerHTML = '&#x2714;';
+                icon.classList.add('text-success');
+                icon.classList.remove('text-danger');
+            }else{
+                icon.innerHTML = '&#x2718;';
+                icon.classList.add('text-danger');
+                icon.classList.remove('text-success');
+            }
+        });
+
+        // Confirm Password
+        const confirmPassword = document.getElementById('password-confirm');
+        confirmPassword.addEventListener('input', function() {
+            const icon = document.getElementById('icon-password-confirm');
+            if (password.value !== confirmPassword.value) {
+                icon.innerHTML = '&#x2718;';
+                icon.classList.add('text-danger');
+                icon.classList.remove('text-success');
+            } else {
+                icon.innerHTML = '&#x2714;';
+                icon.classList.remove('text-danger');
+                icon.classList.add('text-success');
+            }
+        });
+
+        function validateForm(){
+            if (
+                name.value.length < 1 ||
+                lastname.value.length < 1 ||
+                (phoneNumber.value[0] !== '+' || !/^\+\d*$/.test(phoneNumber.value)) ||
+                !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email.value) ||
+                password.value.length < 8 ||
+                password.value !== confirmPassword.value){
+                    return false;
+                }else{
+                    return true;
+                }
         }
+
+
+
     </script>
 
 @endsection
