96643d9981eb2b9ea5dc2d13c20884d59b53cbf6
GiacWebDev <giacobechini@gmail.com>
2024-01-26 13:17:56 +0100

    [edit] footer

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="container mt-5 mb-3">
+        <div class="row">
+
+            <!-- LOGO -->
+            <div class="col-12 col-sm-4 mb-3 mb-sm-0 d-flex justify-content-center justify-content-sm-start logo-footer">
+                <img class="img-fluid" src="../../public/logo (1).png" alt="" />
+            </div>
+
+            <!-- PRIVACY -->
+            <div class="col-12 col-sm-4 mb-3 mb-sm-0 d-flex justify-content-center align-items-center">
+                <ul class="d-flex flex-row list-unstyled m-0 justify-content-center">
+                    <li>
+                        <a href="#">Privacy</a>
+                    </li>
+                    <li>
+                        <a href="#">Condizioni</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- MENU FOOTER -->
+            <div class="col-12 col-sm-4 d-flex align-items-center justify-content-center justify-content-sm-end">
+                <ul class="d-flex flex-row list-unstyled m-0">
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
