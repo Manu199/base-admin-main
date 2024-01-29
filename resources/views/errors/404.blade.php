74821d5999d7af7cf42b6814f1399a8248bb5068
AnotherDebug <antonio.faddanno@gmail.com>
2024-01-29 21:41:38 +0100

    [UPDATE]: style page Error404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-8 offset-md-2 text-center">
                 <h1>Errore - 404!</h1>
                 <p>La pagina che cerchi non è stata trovata.</p>
-                <a href="{{ route('admin.apartment.index') }}" class="btn btn-outline-primary mt-3"><i class="fa-solid fa-arrow-right-to-bracket fa-rotate-180"></i> Torna indietro</a>
+                <a href="{{ route('admin.apartment.index') }}" class="btn btn-outline-primary"><i class="fa-solid fa-arrow-right-to-bracket fa-rotate-180"></i> Torna indietro</a>
             </div>
         </div>
     </div>
