b7218eb952215bf9d65380cb869057ff4b5e04aa
AnotherDebug <antonio.faddanno@gmail.com>
2024-01-31 16:43:57 +0100

    [ADD]: button listMessage

--- resources/views/admin/apartments/listMessages.blade.php
+++ resources/views/admin/apartments/listMessages.blade.php
@@ -5,6 +5,8 @@
     <div class="messagesApartment">
         {{-- Card List Messages --}}
         {{-- @include('admin.partials.card_list_messages') --}}
+        <a href="{{ route('admin.apartment.show', $apartment) }}" class="btn btn-custom-index"><i
+                class="fa-solid fa-arrow-right-to-bracket fa-rotate-180 fs-5"></i> Torna indietro</a>
         @include('admin.partials.box_mail')
     </div>
 @endsection
