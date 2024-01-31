d5c2726e6ad939a394d4d62cb6f4ebce4b80a2ec
AnotherDebug <antonio.faddanno@gmail.com>
2024-01-31 16:52:04 +0100

    [EDIT]: button route listMessage

--- resources/views/admin/apartments/listMessages.blade.php
+++ resources/views/admin/apartments/listMessages.blade.php
@@ -5,8 +5,15 @@
     <div class="messagesApartment">
         {{-- Card List Messages --}}
         {{-- @include('admin.partials.card_list_messages') --}}
-        <a href="{{ route('admin.apartment.show', $apartment) }}" class="btn btn-custom-index"><i
+        @if (Route::currentRouteName() === 'admin.apartments.listMessages')
+        <a href="{{ route('admin.apartment.index') }}" class="btn btn-custom-index"><i
+            class="fa-solid fa-arrow-right-to-bracket fa-rotate-180 fs-5"></i> Torna indietro</a>
+        @else
+            <a href="{{ route('admin.apartment.show', $apartment) }}" class="btn btn-custom-index"><i
                 class="fa-solid fa-arrow-right-to-bracket fa-rotate-180 fs-5"></i> Torna indietro</a>
+        @endif
         @include('admin.partials.box_mail')
+
+
     </div>
 @endsection
